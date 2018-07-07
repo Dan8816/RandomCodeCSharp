e957b1d710755b1b5fb3e74917097e7e5515776c
Dan8816 <dan881631537518@gmail.com>
2018-07-07 15:19:22 -0500

    who is tracking

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RandomCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
